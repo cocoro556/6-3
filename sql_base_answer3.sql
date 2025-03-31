7621886665e300f4bf1a7aa81abd620bd7f64361
cocoro556 <yuto.nakayama0@gmail.com>
2025-03-31 13:35:08 +0900

    インデント修正

--- sql_base_answer3.sql
+++ sql_base_answer3.sql
@@ -1,47 +1,87 @@
 -- 問22
-SELECT id, country_id, uniform_num, position, name, club, birth, height, weight
-FROM players
-ORDER BY height DESC, weight DESC;
+SELECT
+    id,
+    country_id,
+    uniform_num,
+    position,
+    name,
+    club,
+    birth,
+    height,
+    weight
+FROM
+    players
+ORDER BY
+    height DESC,
+    weight DESC;
 
 -- 問23
-SELECT id, country_id, uniform_num, SUBSTRING(position, 1, 1) AS position_initial, name
-FROM players;
+SELECT
+    id,
+    country_id,
+    uniform_num,
+    SUBSTRING(position, 1, 1) AS position_initial,
+    name
+FROM
+    players;
 
 -- 問24
-SELECT name, LENGTH(name) AS name_length
-FROM countries
-ORDER BY name_length DESC;
+SELECT
+    name,
+    LENGTH (name) AS name_length
+FROM
+    countries
+ORDER BY
+    name_length DESC;
 
 -- 問25
-SELECT name, DATE_FORMAT(birth, '%Y年%m月%d日') AS birthday
-FROM players;
+SELECT
+    name,
+    DATE_FORMAT (birth, '%Y年%m月%d日') AS birthday
+FROM
+    players;
 
 -- 問26
-SELECT IFNULL(player_id, 9999) AS player_id, goal_time
-FROM goals;
+SELECT
+    IFNULL (player_id, 9999) AS player_id,
+    goal_time
+FROM
+    goals;
 
 -- 問27
-SELECT 
-    CASE 
+SELECT
+    CASE
         WHEN player_id IS NULL THEN 9999
         ELSE player_id
-    END AS player_id, 
+    END AS player_id,
     goal_time
-FROM goals;
+FROM
+    goals;
 
 -- 問28
-SELECT AVG(height) AS 平均身長, AVG(weight) AS 平均体重
-FROM players;
+SELECT
+    AVG(height) AS 平均身長,
+    AVG(weight) AS 平均体重
+FROM
+    players;
 
 -- 問29
-SELECT COUNT(*) AS 日本のゴール数
-FROM goals
-WHERE player_id BETWEEN 714 AND 736;
+SELECT
+    COUNT(*) AS 日本のゴール数
+FROM
+    goals
+WHERE
+    player_id BETWEEN 714 AND 736;
 
 -- 問30
-SELECT COUNT(player_id) AS オウンゴール以外のゴール数
-FROM goals;
+SELECT
+    COUNT(player_id) AS オウンゴール以外のゴール数
+FROM
+    goals;
 
 -- 問31
-SELECT MAX(height) AS 最大身長, MAX(weight) AS 最大体重
-FROM players;
+SELECT
+    MAX(height) AS 最大身長,
+    MAX(weight) AS 最大体重
+FROM
+    players;
